e8dff2c550d1ff2d9569c38ada26e361622c4f8e
Gabriel Farias <gabrielf6793@gmail.com>
2025-02-16 18:18:47 -0300

    up exercicios

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 70
+console.log(`a velocidade do seu carro ${vel} km/h`)
+
+if( vel > 60){
+    console.log('voce ultrapassou a velocidade permitida MULTADO')
+}
+
+console.log('dirija sempre usando sinto de segurança')
---
+++ ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+
+if(pais == 'BRA'){
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
